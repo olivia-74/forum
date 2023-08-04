62e862d3e2a09e9608bf0ed4752accd6ef2c514c
Danielchini05 <84184738+Danielchini05@users.noreply.github.com>
2023-08-04 12:36:49 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import Perfil from './pages/perfil/Perfil';
+import PaginaInicial from './pages/páginaInicial/PaginaInicial';
+
+function App() {
+  return (
+    <>
+    <PaginaInicial/>
+    <Cadastro/>
+    <Home/>
+    <Perfil/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/perfil/Perfil.js
@@ -0,0 +1,9 @@
+
+function Perfil(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default Perfil
