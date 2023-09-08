5bf36b8e0ca089a217974afaa33e23fe1cbf95c4
Danielchini05 <84184738+Danielchini05@users.noreply.github.com>
2023-09-08 12:32:03 -0300

    Add files via upload

--- forum/src/componentes/rotas/Rotas.js
+++ forum/src/componentes/rotas/Rotas.js
@@ -1,6 +1,5 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import PaginaInicial from "../paginas/Inicial/PaginaInicial";
-import SignIn from "../paginas/SignIn/Signin";
 import LogIn from "../paginas/Login/Login";
 import Home from "../paginas/Home/Home";
 import Discussao from "../paginas/Discussao/Discussao";
@@ -10,10 +9,10 @@ function Rotas (){
     return (
         <BrowserRouter>
         <Routes>
-            <Route path="*" element={<Erro/>}/>
+            {/* <Route path="*" element={<Erro/>}/> */}
             <Route index element={<PaginaInicial/>}/>
             <Route path="Login" element={<LogIn/>}/> 
-            <Route path="Signin" element={<SignIn/>}/>
+            
             <Route path="Home" element={<Home/>}/>
             <Route path="Discussao" element={<Discussao/>}/>
 
